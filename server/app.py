4d79cd18c4fabb6819b2a2ddd1a9982845a79dd0
Venkatesh Pamudurti <pamudurtivenkatesh@gmail.com>
2026-04-08 15:53:25 +0530

    Fix server entry points for OpenEnv

--- server/app.py
+++ server/app.py
@@ -5,8 +5,11 @@ import uvicorn
 
 app = FastAPI(title="AI Data Cleaner API for OpenEnv Grader")
 
-def start():
+def main():
     uvicorn.run("server.app:app", host="0.0.0.0", port=7860)
+
+if __name__ == '__main__':
+    main()
 
 current_env = None
 
